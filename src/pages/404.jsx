8c26a407697d73557953ee4ffa47f7f2b169cc83
Kristóf Poduszló <kripod@protonmail.com>
2018-01-28 20:35:40 +0100

    Add missing title for the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import Container from '../components/Container';
 
 const NotFoundPage = () => (
   <Container>
+    <Helmet title="Hiba" />
+
     <h1>Az oldal nem található</h1>
     <p>A kért oldal jelenleg nem elérhető vagy nem létezik.</p>
   </Container>

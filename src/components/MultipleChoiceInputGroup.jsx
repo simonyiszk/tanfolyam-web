2712aa2f3b38caf9178546843a6cfbff363ff04b
Balázs Frey <balazs.frey@gmail.com>
2018-02-07 20:04:59 +0100

    Change buttons style

--- src/components/MultipleChoiceInputGroup.jsx
+++ src/components/MultipleChoiceInputGroup.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styles from './MultipleChoiceInputGroup.module.scss';
 
-const MultipleChoiceInputGroup = ({ name, children, ...props }) => (
-  <div {...props}>
+const MultipleChoiceInputGroup = ({
+  name, children, className, ...props
+}) => (
+  <div className={`${styles.root} ${className}`} {...props}>
     {React.Children.map(children, (input, i) =>
       React.cloneElement(input, {
         id: input.props.id != null ? input.props.id : `${name}__${i}`,
@@ -14,6 +17,10 @@ const MultipleChoiceInputGroup = ({ name, children, ...props }) => (
 MultipleChoiceInputGroup.propTypes = {
   name: PropTypes.string.isRequired,
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  className: PropTypes.string,
 };
 
+MultipleChoiceInputGroup.defaultProps = {
+  className: '',
+};
 export default MultipleChoiceInputGroup;
